4df8cf627f555bed45e84905fd3ce3e50e32437b
mouradaz <moradazoux1@gmail.com>
2024-07-01 21:20:50 +0100

    second commit

---
+++ PHP/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="..//styles.css">
+    <title>Mourad store</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="..//images/logo.jpg" alt="logo">
+            <span id="s1">Mourad Store</span>
+        </div>
+        
+        <nav>
+            <ul>
+                <li><a href="..//html/file.html">Home</a></li>
+                <li><a href="..//html/about.html">About</a></li>
+                <li><a href="..//html/catalog.html">catalogue</a></li>
+                <li><a href="..//html/contact .html">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="main3" id="main4">
+        <div class="contact-container">
+            <h1>Inscription :</h1>
+            <form class="contact-form" action="conx.php" method="post">
+                <fieldset class="fieldset">
+                    <legend>formulaire d'inscription : </legend>
+                <label for="name">Nom:</label><br>
+                <input type="text" id="name" name="name" required><br>
+                <label for="email">Email:</label><br>
+                <input type="email" id="email" name="email" required><br>
+                <label for="password">password:</label><br>
+                <textarea id="password" name="password" required></textarea><br>
+                <button id="btn2" type="submit">Envoyer</button>
+            </fieldset>
+            </form>
+        </div>
+        
+        
+       
+    </main>
+    <footer>
+        <div class="contact">
+            <h2>Contactez-nous</h2>
+            <div class="contact-info">
+                <img src="logo.jpg" alt="logo" class="footer-logo">
+                <p>Email : <a href="mailto:mouradazougagh61@gmail.com">mouradazougagh61@gmail.com</a></p>
+                <p>Téléphone : <a href="tel:+212705220576">+212 705220576</a></p>
+                <p>Adresse : RES TAFOUKT FADISSA AGADIR MAROC</p>
+            </div>
+            <div class="social-media">
+                <a href="https://wa.me/212705220576" target="_blank">WhatsApp</a>
+                <a href="https://web.facebook.com/morad.azougargh/" target="_blank">Facebook</a>
+                <a href="https://www.instagram.com/mrd_a_z" target="_blank">Instagram</a>
+            </div>
+            <div class="additional-info">
+                <h3>Autres Informations</h3>
+                <p><strong>Problèmes :</strong> Si vous rencontrez des problèmes avec votre commande, veuillez nous contacter immédiatement.</p>
+                <p><strong>Méthodes de paiement :</strong>Paiement à la livraison.</p>
+            </div>
+        </div>
+        <p>&copy; 2024 t-shirt By Mourad Store</p>
+    </footer>
+</body>
+</html>
+
+
+    
+
+    <script src="..//page.js"></script>
+</body>
+</html>
--- page.js
+++ page.js
@@ -1,7 +1,7 @@
 var btn1 = document.querySelector("#btn1");
 
 btn1.addEventListener("click",function(){
-    var main = document.querySelector("#main3")
+    var main = document.querySelector(".main3")
     var div1 = document.querySelector(".contact-container");
     div1.style.display="none";
     var h1 = document.createElement("h1");
@@ -15,3 +15,23 @@ btn1.addEventListener("click",function(){
     main.appendChild(p);
 
 })
+
+var btn2= document.querySelector("#btn2");
+
+btn2.addEventListener("click",function(){
+    var main = document.querySelector(".main4")
+    var div1 = document.querySelector(".contact-container");
+    div1.style.display="none";
+    var h1 = document.createElement("h1");
+    h1.textContent = "Formulaire soumis avec succès !";
+    h1.style.color="green";
+    main.appendChild(h1);
+    h1.style.textAlign="center";
+    var p = document.createElement("p");
+    p.textContent = "Merci de nous avoir contactés. Nous vous répondrons dans les plus brefs délais.";
+    p.style.textAlign="center";
+    main.appendChild(p);
+
+})
+
+
